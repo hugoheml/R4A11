9a74f9f977f74b9d1bd726f9a4e829f78ceea430
hugoheml <hugo-hermelin17@orange.fr>
2025-01-31 16:35:08 +0100

    feat(tp1): start first part

---
+++ app/src/main/java/com/example/tp2/MainActivity.kt
@@ -0,0 +1,88 @@
+package com.example.tp2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tp2.ui.theme.TP2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TP2Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column (
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        var inputText by remember { mutableStateOf("Jean-François") }
+
+                        TitleText(name = "Bienvenue")
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Text(inputText)
+                        Spacer(modifier = Modifier.height(4.dp))
+                        TextField(
+                            value = inputText,
+                            onValueChange = { inputText = it },
+                            label = { Text("Saisir votre nom") }
+                        )
+                        Spacer(modifier = Modifier.height(4.dp))
+                        Button(
+                            onClick = { },
+                            content = {
+                                Text("Valider")
+                            }
+                        )
+
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TitleText(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = name,
+        modifier = modifier,
+        textAlign = TextAlign.Center,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TP2Theme {
+        TitleText("Androidddddqsqsqsd")
+    }
+}
