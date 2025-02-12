ca6728b268832c650486790fb38cdba9e2f5debf
Lucas Bocquet <159040687+lbocquet0@users.noreply.github.com>
2025-02-12 09:54:27 +0100

    feat(main-page): add main page

--- shifumi/app/src/main/java/fr/unilim/iut/shifumi/MainActivity.kt
+++ shifumi/app/src/main/java/fr/unilim/iut/shifumi/MainActivity.kt
@@ -4,13 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import fr.unilim.iut.shifumi.ui.theme.ShifumiTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +34,65 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ShifumiTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    color = colorResource(id = R.color.primary)
+                ) {
+                    App_Navigation()
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun App_Navigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home") {
+            HomePage(navController = navController)
+        }
+    }
+}
+
+@Composable
+fun HomePage(navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        GameTitle()
+        GameLogo()
+        Spacer(modifier = Modifier.height(24.dp));
+        Button(onClick = {
+            navController.navigate("form") },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = colorResource(id = R.color.secondary),
+            ),
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(text = "Jouer")
+        }
+    }
+}
+
+@Composable
+fun GameTitle() {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Shifumi",
+        style = MaterialTheme.typography.titleLarge
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ShifumiTheme {
-        Greeting("Android")
-    }
+fun GameLogo() {
+    Image(
+        painter = painterResource(id = R.drawable.app_logo),
+        contentDescription = "Shifumi logo"
+    )
 }
